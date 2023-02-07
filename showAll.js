92cf28b525ba7201f3fb26d147b976ae5cec7bc7
Daniel Hatas <daniel.hatas@codecool.com>
2023-02-07 10:27:34 +0100

    remove showAll content

--- showAll.js
+++ showAll.js
@@ -1,10 +1,6 @@
 const getBeers = require('./data')
 
 function main() {
-    const beers = getBeers();
-    beers.forEach((beer) => {
-        console.log(`${beer.name}  (${beer.brewery})` )
-    })
 }
 
 main()
